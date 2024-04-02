e5e59e9bf39acd60002bae90a0122bed0690bcf2
Jana Bazarbekovic <bazar.akhmet@gmail.com>
2024-04-02 15:32:47 +0500

    very small commit

--- v3/inner_registry.py
+++ v3/inner_registry.py
@@ -90,7 +90,7 @@ def add_coordinators_v4(sheet):
     # Add directors (final piece)
     sheet['B2'] = directors[0]
     sheet['B4'] = directors[1]
-    sheet['H11'] = ''
+    sheet['H11'] = '' # remove the temporary company name
 
 def loop_json(json_data, workbook):
     '''
