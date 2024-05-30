176dd1907e35b972c9a6dc645c9b6cbcca7d5b3d
Jana Bazarbekovic <bazar.akhmet@gmail.com>
2024-05-30 17:41:31 +0500

    added shortnames for some document types

---
+++ v3/shortnames.py
@@ -0,0 +1,7 @@
+shortenings = {
+    'Zayavka na oplatu': 'oplaty',
+    'Zayavka na nalogi': 'nalogi',
+    'Zayavka na zarplatu': 'zarplaty',
+    'Zayavka na peretok': 'peretoki',
+    # Add more mappings as needed
+}
