f1b90097ffd546eee39d61806dda0b7e89e17df7
Jana Bazarbekovic <bazar.akhmet@gmail.com>
2024-09-23 12:31:13 +0500

    new version of report creator

--- v3/shortnames.py
+++
@@ -1,7 +0,0 @@
-shortenings = {
-    'Zayavka na oplatu': 'oplaty',
-    'Zayavka na nalogi': 'nalogi',
-    'Zayavka na zarplatu': 'zarplaty',
-    'Zayavka na peretok': 'peretoki',
-    # Add more mappings as needed
-}
